ccc161d8822c793a797b6e93830b48186a14fce7
Ayush Kumar Rai <66361024+DevNoob1@users.noreply.github.com>
2024-11-15 18:50:02 +0530

    Update Middle.js

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -6,6 +6,10 @@ const Middle = () => {
     return (
         <div className="Middle">
             <Services />
+        <div className='w-[80%] m-auto mt-28 flex flex-col md:flex-row  items-center gap-10'>
+                <h1 className='text-4xl text-center sm:text-left font-bold p-2 lg:w-[40%] bg-[#B9FF66]'>Our Working Process </h1>
+                <p className='text-center sm:text-left '>Step-by-Step Guide to Achieving <br /> Your Business Goals</p>
+            </div>
             <Process />
         </div>
     );
